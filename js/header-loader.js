312358aff08b8d4a4d96a514d9ed4ff4aa50e82e
Acnolend <tonio.lol10@gmail.com>
2024-05-05 12:12:06 +0100

    Cambiando contenido del main

--- js/header-loader.js
+++
@@ -1,9 +0,0 @@
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementsByClassName('header')[0].innerHTML = data;
-        const scriptElement = document.createElement('script');
-        scriptElement.src = 'js/toolbar-authentication.js';
-        document.body.appendChild(scriptElement);
-    })
-    .catch(error => console.error('Error al cargar el header: ', error));
